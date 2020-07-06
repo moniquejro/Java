3a5bd7c0d1c33699c516d21ebfe98a3bb9b63b4c
unknown <moniquejroliveira@gmail.com>
2020-07-06 15:47:20 -0300

    média de um aluno

---
+++ ex05a.java
@@ -0,0 +1,20 @@
+package PacoteJava;
+
+public class ex05a 
+{
+
+	public static void main(String[] args) 
+	{
+		int n1, n2, n3, media;
+		
+		System.out.println("Vamos calcular a média de um aluno, com notas: \n1ª: 2\n2ª: 3\n3ª: 5");
+		
+		n1 = 2;
+		n2 = 3;
+		n3 = 5;
+		media = (n1 + n2 + n3)/3;
+		
+		System.out.println("\nA média é igual a "+media);
+	}
+
+}
